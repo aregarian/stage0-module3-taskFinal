cf75a2e86aa638e5d0771970397cd3d81636d047
Fanny Dorthey <fannydor@ev-k6.kzn.21-school.ru>
2023-10-18 22:28:09 +0300

    last push will be

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,6 +2,6 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
-        System.out.println((number % 10) + ((number % 100) /10) + (number / 100));
+        System.out.println((number % 10) + "" + ((number % 100) /10) + "" + (number / 100));
     }
 }
